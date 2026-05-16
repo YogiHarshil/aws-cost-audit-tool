8c4968edba33e321f1181ac2ae579b145c1317a9
Harshil Yogi <harshilyogi077@gmail.com>
2026-05-16 11:11:52 +0530

    fix: update scanners/__init__.py exports, remove env file with credentials

--- scanners/__init__.py
+++ scanners/__init__.py
@@ -1,12 +1,17 @@
 """AWS resource scanners for cost audit findings."""
 
 from scanners.base import BaseScanner
+from scanners.compute_optimizer import scan_compute_optimizer
 from scanners.cost_explorer import CostExplorerScanner
 from scanners.ebs import EBSScanner
 from scanners.ec2 import EC2Scanner
 from scanners.eip import EIPScanner
 from scanners.rds import RDSScanner
+from scanners.reservations import ReservationScanner
 from scanners.s3 import S3Scanner
+from scanners.savings_plans import scan_savings_plans
+from scanners.snapshots import SnapshotScanner
+from scanners.trusted_advisor import scan_trusted_advisor
 
 __all__ = [
     "BaseScanner",
@@ -15,5 +20,10 @@ __all__ = [
     "EC2Scanner",
     "EIPScanner",
     "RDSScanner",
+    "ReservationScanner",
     "S3Scanner",
+    "SnapshotScanner",
+    "scan_compute_optimizer",
+    "scan_savings_plans",
+    "scan_trusted_advisor",
 ]
